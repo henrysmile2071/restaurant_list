705ec7b96aa3d76e5159ab485f207b953fc1cca9
henrysmile2071 <henrychen.chc@gmail.com>
2022-09-05 23:24:27 +0800

    feat add mongoose

--- app.js
+++ app.js
@@ -4,16 +4,30 @@
 //packages and constants
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI2, { useNewUrlParser: true, useUnifiedTopology: true })
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+
 //template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //static files
 app.use(express.static('public'))
 
+// connect to database
+const db = mongoose.connection
+// Error
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// Connection success
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 //Routes
+
 //index('/')
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
@@ -22,7 +36,7 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
   const restaurants = restaurantList.results.filter(restaurant => {
-   return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
   })
   res.render('index', { restaurants, keyword })
 })
