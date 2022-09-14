d9116a55552cb08d01d46105e31c7f18a1c35258
henrysmile2071 <henrychen.chc@gmail.com>
2022-09-14 15:41:04 +0800

    feat: add method-override and modify route names

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI2, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const port = 3000
+const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const hbsHelpers = exphbs.create({
   helpers: require('./utility/handlebarsHelper').helpers,
@@ -24,7 +25,8 @@ app.use(express.static('public'))
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: true }))
-
+//method-override
+app.use(methodOverride('_method'))
 // connect to database
 const db = mongoose.connection
 // Error
@@ -37,8 +39,8 @@ db.once('open', () => {
 })
 
 //Routes
-//to add restaurant page('/restaurant/new')
-app.get('/restaurant/new', (req, res) => {
+//to add restaurant page('/restaurants/new')
+app.get('/restaurants/new', (req, res) => {
   res.render('new')
 })
 
@@ -53,6 +55,7 @@ app.post('/restaurants', (req, res) => {
 //show all restaurants index('/')
 app.get('/', (req, res) => {
   Restaurant.find()
+    .collation({ locale: "en" })
     .lean()
     .sort({ name: 'desc' })
     .then(restaurants => {
@@ -68,6 +71,7 @@ app.get('/search', (req, res) => {
   const sortBy = sortMethods.find(x => x.text === selected).method
   const searchQuery = { $text: { $search: keyword } } //https://stackoverflow.com/questions/28775051/best-way-to-perform-a-full-text-search-in-mongodb-and-mongoose
   return Restaurant.find(searchQuery)
+    .collation({ locale: "en" })//fix for MongoDB case-insensitive sorting
     .lean()
     .sort(sortBy)
     .then(restaurants => {
@@ -77,33 +81,33 @@ app.get('/search', (req, res) => {
     .catch(error => console.log(error))
 })
 
-//show details('/restaurants/:restaurant_id')
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  return Restaurant.findById(req.params.restaurant_id)
+//show details('/restaurants/:_id')
+app.get('/restaurants/:_id', (req, res) => {
+  return Restaurant.findById(req.params._id)
     .lean()
     .then((restaurants) =>
       res.render('show', { restaurants }))
     .catch(error => console.log(error))
 })
 
-//to edit restaurant page('/restaurants/:restaurant_id/edit')
-app.get('/restaurants/:restaurant_id/edit', (req, res) => {
-  return Restaurant.findById(req.params.restaurant_id)
+//to edit restaurant page('/restaurants/:_id/edit')
+app.get('/restaurants/:_id/edit', (req, res) => {
+  return Restaurant.findById(req.params._id)
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
 })
 
-//post edited restaurant ('/restaurants/:restaurant_id)
-app.post('/restaurants/:restaurant_id/edit', (req, res) => {
-  const id = req.params.restaurant_id
+//post edited restaurant ('/restaurants/:_id)
+app.put('/restaurants/:_id', (req, res) => {
+  const id = req.params._id
   return Restaurant.findByIdAndUpdate(id, req.body)
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
 
 //delete restaurant
-app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+app.delete('/restaurants/:_id/delete', (req, res) => {
   const id = req.params.restaurant_id
   return Restaurant.findById(id)
     .then(restaurant => restaurant.remove())
