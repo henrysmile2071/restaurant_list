6ed573d584dca95378b794365f324f0071aca251
henrysmile2071 <henrychen.chc@gmail.com>
2022-11-05 19:22:09 +0800

    fix: fix route typo

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -7,7 +7,7 @@ router.get('/facebook', passport.authenticate('facebook', {
   scope: ['email', 'public_profile']
 }))
 
-router.get('/facebook/callbook', passport.authenticate('facebook', {
+router.get('/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
